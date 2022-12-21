126e3d2a9ceed5902b435e5c60d203cdcbe422b1
loctvl842 <loclepnvx@gmail.com>
2022-12-21 14:09:38 +0700

    update

---
+++ lua/lightline/colorscheme/monokai-pro.lua
@@ -0,0 +1,51 @@
+local filter = vim.g.monokai_pro_filter
+local colors = require("monokai-pro.themes.monokai-" .. filter)
+
+local monokai_pro = {}
+
+monokai_pro.normal = {
+  left = {
+    { colors.base.black, colors.base.yellow },
+    { colors.base.yellow, colors.editorSuggestWidget.background },
+  },
+  middle = { { colors.base.black, colors.base.black } },
+  right = {
+    { colors.base.black, colors.base.yellow },
+    { colors.base.yellow, colors.editorSuggestWidget.background },
+  },
+  error = { { colors.base.black, colors.base.red } },
+  warning = { { colors.base.black, colors.base.blue } },
+}
+
+monokai_pro.insert = {
+  left = {
+    { colors.base.black, colors.base.green },
+    { colors.base.green, colors.editorSuggestWidget.background },
+  },
+}
+
+monokai_pro.visual = {
+  left = {
+    { colors.base.black, colors.base.magenta },
+    { colors.base.magenta, colors.editorSuggestWidget.background },
+  },
+}
+
+monokai_pro.replace = {
+  left = {
+    { colors.base.black, colors.base.red },
+    { colors.base.red, colors.editorSuggestWidget.background },
+  },
+}
+
+monokai_pro.inactive = {
+  left = {
+    { colors.base.yellow, colors.base.black },
+    { colors.base.black, colors.base.black },
+  },
+  middle = {
+    { colors.base.black, colors.base.black },
+  },
+}
+
+return monokai_pro
