00305cc8d71dd8e0ab5db41c8e0f9b01d624e393
loctvl842 <loclepnvx@gmail.com>
2022-12-25 20:32:33 +0700

    refactor

--- lua/monokai-pro/init.lua
+++ lua/monokai-pro/init.lua
@@ -2,7 +2,7 @@ local M = {}
 
 vim.api.nvim_command("hi clear")
 if vim.fn.exists("syntax_on") then
-  vim.api.nvim_command("syntax reset")
+	vim.api.nvim_command("syntax reset")
 end
 
 local util = require("monokai-pro.util")
@@ -14,143 +14,86 @@ vim.o.termguicolors = true
 vim.g.colors_name = "monokai-pro"
 vim.g.monokai_pro_config = Config
 
-local function highlightBufferLineIcon(theme_palette, config)
-  local icon_ok, webDevicons = pcall(require, "nvim-web-devicons")
-  if icon_ok then
-    vim.api.nvim_create_autocmd("BufEnter", {
-      pattern = "*",
-      callback = function()
-        local ft = vim.bo.filetype
-        local _, icon_name = webDevicons.get_icon_by_filetype(ft, { default = true })
-        local _, icon_color = webDevicons.get_icon_color_by_filetype(ft, { default = true })
-        if not icon_name then
-          return
-        end
-        local iconSkeleton = {
-          ["BufferLine" .. icon_name .. "Selected"] = {
-            bg = config.transparent_background and "NONE" or theme_palette.tab.activeBackground,
-            fg = icon_color,
-            sp = theme_palette.tab.activeBorder,
-            underline = config.plugins.bufferline.underline_selected
-          },
-          ["BufferLine" .. icon_name] = {
-            bg = theme_palette.tab.inactiveBackground,
-            fg = icon_color,
-          },
-          ["BufferLine" .. icon_name .. "Inactive"] = {
-            bg = config.transparent_background and "NONE" or theme_palette.tab.unfocusedActiveBackground,
-            fg = icon_color,
-            sp = theme_palette.tab.unfocusedActiveBorder,
-            underline = config.plugins.bufferline.underline_selected
-          },
-        }
-        util.initialise(iconSkeleton)
-      end,
-    })
-  end
-end
+M.plugins = {
+	"StatusLine",
+	"alpha",
+	"breadcrumb",
+	"bufferline",
+	"cmp",
+	"coc",
+	"gitsign",
+	"illuminate",
+	"indent_blankline",
+	"lsp",
+	"neo-tree",
+	"notify",
+	"nvim-navic",
+	"nvim-tree",
+	"nvim-treesitter",
+	"packer",
+	"renamer",
+	"scrollbar",
+	"telescope",
+	"toggleterm",
+	"ufo",
+	"which-key",
+	"wilder",
+}
 
+-- gnerate the colorscheme
 local function generate(theme)
-  C = vim.tbl_deep_extend("force", C, theme)
-
-  local plugins = {
-    "StatusLine",
-    "alpha",
-    "breadcrumb",
-    "bufferline",
-    "cmp",
-    "coc",
-    "gitsign",
-    "illuminate",
-    "indent_blankline",
-    "lsp",
-    "neo-tree",
-    "notify",
-    "nvim-navic",
-    "nvim-tree",
-    "nvim-treesitter",
-    "packer",
-    "renamer",
-    "scrollbar",
-    "telescope",
-    "toggleterm",
-    "ufo",
-    "which-key",
-    "wilder",
-  }
-
-  local editor = require("monokai-pro.editor")
-  local syntax = require("monokai-pro.syntax")
-  local semantic_tokens = require("monokai-pro.semantic_tokens")
-  local special = require("monokai-pro.special")
-  local skeletons = {
-    ["editor"] = editor,
-    ["syntax"] = syntax,
-    ["special"] = special,
-    ["semantic_tokens"] = semantic_tokens,
-  }
-
-  for _, p in ipairs(plugins) do
-    local plugin_ok, pluginConfig = pcall(require, "monokai-pro.plugins." .. p)
-    if not plugin_ok then
-      local msg = "There is no highlight config named " .. p
-      local level = "warn"
-      util.notify(msg, level)
-      goto continue
-    end
-    skeletons[p] = pluginConfig
-    ::continue::
-  end
-
-  for skeleton_name, skeleton_config in pairs(skeletons) do
-    local status_ok, _ = pcall(util.initialise, skeleton_config)
-    if not status_ok then
-      local msg = "Highlight failed in skeleton " .. skeleton_name
-      local level = "error"
-      util.notify(msg, level)
-      goto continue
-    end
-    ::continue::
-  end
-  highlightBufferLineIcon(C, Config)
+	C = vim.tbl_deep_extend("force", C, theme)
+	local skeletons = util.load_skeletons(M.plugins)
+	for skeleton_name, skeleton_config in pairs(skeletons) do
+		local status_ok, _ = pcall(util.initialise, skeleton_config)
+		if not status_ok then
+			local msg = "Highlight failed in skeleton " .. skeleton_name
+			local level = "error"
+			util.notify(msg, level)
+			goto continue
+		end
+		::continue::
+	end
+	util.highlightBufferLineIcon(C, Config)
 end
 
 local function create_filter_command()
-  local cmd = vim.api.nvim_create_user_command
-  cmd("MonokaiProSelect", function()
-    local menu = util.create_menu("Set monokai filter",
-      { "classic", "octagon", "pro", "machine", "ristretto", "spectrum" },
-      function(item)
-        local filter = item.value
-        Config.filter = filter
-        vim.g.monokai_pro_config = Config
-        M.setup(Config)
-      end)
-    menu:mount()
-  end, { nargs = 0 })
-  cmd("MonokaiPro", function(opts)
-    local filter = opts.args
-    Config.filter = filter
-    vim.g.monokai_pro_config = Config
-    M.setup(Config)
-  end, { nargs = 1 })
+	local cmd = vim.api.nvim_create_user_command
+	cmd("MonokaiProSelect", function()
+		local menu = util.create_menu(
+			"Set monokai filter",
+			{ "classic", "octagon", "pro", "machine", "ristretto", "spectrum" },
+			function(item)
+				local filter = item.value
+				Config.filter = filter
+				vim.g.monokai_pro_config = Config
+				M.setup(Config)
+			end
+		)
+		menu:mount()
+	end, { nargs = 0 })
+	cmd("MonokaiPro", function(opts)
+		local filter = opts.args
+		Config.filter = filter
+		vim.g.monokai_pro_config = Config
+		M.setup(Config)
+	end, { nargs = 1 })
 end
 
-vim.g.monokai_pro_config = {}
 function M.setup(user_config)
-  Config = vim.tbl_deep_extend("force", Config, user_config)
-  vim.g.monokai_pro_config = Config
-  local filter = Config.filter == "" and "pro" or Config.filter
-  local theme_palette = require('monokai-pro.themes.monokai-' .. filter)
-  generate(theme_palette)
-  create_filter_command()
-  vim.cmd([[colorscheme monokai-pro]])
+	Config = vim.tbl_deep_extend("force", Config, user_config)
+	vim.g.monokai_pro_config = Config
+	local filter = Config.filter == "" and "pro" or Config.filter
+	local theme_palette = require("monokai-pro.themes.monokai-" .. filter)
+	generate(theme_palette)
+	create_filter_command()
+	vim.cmd([[colorscheme monokai-pro]])
 end
 
 function M.get_base_color()
-  local filter = vim.g.monokai_pro_config.filter or "pro"
-  local theme_palette = require('monokai-pro.themes.monokai-' .. filter)
-  return theme_palette.base
+	local filter = vim.g.monokai_pro_config.filter or "pro"
+	local theme_palette = require("monokai-pro.themes.monokai-" .. filter)
+	return theme_palette.base
 end
 
 return M
--- lua/monokai-pro/util.lua
+++ lua/monokai-pro/util.lua
@@ -1,6 +1,9 @@
 local M = {}
+local Menu = require("nui.menu")
+local NuiText = require("nui.text")
 
--- @param string
+-- convert hex color string to a table rgb `{r: ,g: ,b: }`
+---@param hexcolor string
 local function hexToRgb(hexcolor)
 	hexcolor = string.lower(hexcolor)
 	return {
@@ -10,14 +13,71 @@ local function hexToRgb(hexcolor)
 	}
 end
 
--- @param red, green, blue, alpha string
--- @param background background color behind the blended color
-function M.rgba(red, green, blue, alpha, background)
-	background = hexToRgb(background)
+-- turn `8-digit` hex color to `6-digit` hex color 
+---@param hexColor string
+---@param background string
+function M.hexExtend(hexColor, background)
+	local hex6 = string.sub(hexColor, 1, 7)
+	local alpha = tonumber(string.sub(hexColor, 8, 9), 16) / 255
+	return M.blend(hex6, background, alpha)
+end
+
+-- turn `8-digit` hex color to `6-digit` hex color but with may miss base
+---@param colorValue string
+---@param base string
+---@return string|nil
+local function getRealColor(colorValue, base)
+	if colorValue == nil or string.len(colorValue) ~= 9 then
+		return colorValue
+	end
+	if base == nil then
+		base = C.editor.background
+	end
+	if string.len(base) == 9 then
+		base = M.hexExtend(base, C.editor.background)
+	end
+	return M.hexExtend(colorValue, base)
+end
+
+-- turn the custom `hl_config` into `hl_config` used in `nvim.api.nvim_set_hl`
+---@param hl_config string
+---@return string
+local function generate_hl_val(hl_config)
+	local bg_value = getRealColor(hl_config.bg, hl_config.bg_base)
+	local fg_value = getRealColor(hl_config.fg, hl_config.fg_base)
+	local sp_value = getRealColor(hl_config.sp, bg_value)
+	hl_config.bg = bg_value
+	hl_config.fg = fg_value
+	hl_config.sp = sp_value
+	hl_config.bg_base = nil
+	hl_config.fg_base = nil
+	hl_config.style = nil
+	return hl_config
+end
+
+-- create `highlight group`
+---@param group string
+---@param properties table
+local function highlight(group, properties)
+	local link = properties.link
+	if link ~= nil then
+		local cmd = "hi! link " .. group .. " " .. link
+		vim.api.nvim_command(cmd)
+		return
+	end
+	local hl_val = generate_hl_val(properties)
+	vim.api.nvim_set_hl(0, group, hl_val)
+end
+
+-- generate `hex color` based `rgba` function
+---@param alpha number
+---@param bg_hex string
+function M.rgba(red, green, blue, alpha, bg_hex)
+	local bg_rgb = hexToRgb(bg_hex)
 	-- new color
-	red = (1 - alpha) * background.r + alpha * red
-	green = (1 - alpha) * background.g + alpha * green
-	blue = (1 - alpha) * background.b + alpha * blue
+	red = (1 - alpha) * bg_rgb.r + alpha * red
+	green = (1 - alpha) * bg_rgb.g + alpha * green
+	blue = (1 - alpha) * bg_rgb.b + alpha * blue
 	-- rgb to hex
 	red = string.format("%02x", red)
 	green = string.format("%02x", green)
@@ -25,19 +85,13 @@ function M.rgba(red, green, blue, alpha, background)
 	return "#" .. red .. green .. blue
 end
 
--- @param hexcolor color in hex
--- @param background background color behind the blended color
+-- blend a hex color. For example to have `opacity: 0.8`, we use `blend("#000000", 0.8)`
+---@param hexcolor string
+---@param background string
+---@param alpha number
 function M.blend(hexColor, background, alpha)
 	local rgb = hexToRgb(hexColor)
 	return M.rgba(rgb.r, rgb.g, rgb.b, alpha, background)
-end
-
--- @param hexColor color with eight digit
--- @param background background color behind the blended color
-function M.hexExtend(hexColor, background)
-	local hex6 = string.sub(hexColor, 1, 7)
-	local alpha = tonumber(string.sub(hexColor, 8, 9), 16) / 255
-	return M.blend(hex6, background, alpha)
 end
 
 function M.lightenDarkenColor(color, amt)
@@ -55,43 +109,6 @@ function M.lightenDarkenColor(color, amt)
 	local green = string.format("%02x", color.g)
 	local blue = string.format("%02x", color.b)
 	return "#" .. red .. green .. blue
-end
-
-local function getRealColor(colorValue, base)
-	if colorValue == nil or string.len(colorValue) ~= 9 then
-		return colorValue
-	end
-	if base == nil then
-		base = C.editor.background
-	end
-	if string.len(base) == 9 then
-		base = M.hexExtend(base, C.editor.background)
-	end
-	return M.hexExtend(colorValue, base)
-end
-
-local function generate_hl_val(hl_config)
-	local bg_value = getRealColor(hl_config.bg, hl_config.bg_base)
-	local fg_value = getRealColor(hl_config.fg, hl_config.fg_base)
-	local sp_value = getRealColor(hl_config.sp, bg_value)
-	hl_config.bg = bg_value
-	hl_config.fg = fg_value
-	hl_config.sp = sp_value
-	hl_config.bg_base = nil
-	hl_config.fg_base = nil
-	hl_config.style = nil
-	return hl_config
-end
-
-local function highlight(group, properties)
-	local link = properties.link
-	if link ~= nil then
-		local cmd = "hi! link " .. group .. " " .. link
-		vim.api.nvim_command(cmd)
-		return
-	end
-	local hl_val = generate_hl_val(properties)
-	vim.api.nvim_set_hl(0, group, hl_val)
 end
 
 -- @param highlight group to get (string)
@@ -125,14 +142,16 @@ function M.notify(msg, level, opts)
 	end)
 end
 
+---@param skeleton table
 function M.initialise(skeleton)
 	for group, properties in pairs(skeleton) do
 		highlight(group, properties)
 	end
 end
 
-local Menu = require("nui.menu")
-local NuiText = require("nui.text")
+---@param title string
+---@param items table
+---@param handler function
 function M.create_menu(title, items, handler)
 	local menuItems = {
 		Menu.separator("", {
@@ -173,6 +192,72 @@ function M.create_menu(title, items, handler)
 		on_submit = handler,
 	})
 	return menu
+end
+
+---@param theme_palette table
+---@param config table
+function M.highlightBufferLineIcon(theme_palette, config)
+  local icon_ok, webDevicons = pcall(require, "nvim-web-devicons")
+  if icon_ok then
+    vim.api.nvim_create_autocmd("BufEnter", {
+      pattern = "*",
+      callback = function()
+        local ft = vim.bo.filetype
+        local _, icon_name = webDevicons.get_icon_by_filetype(ft, { default = true })
+        local _, icon_color = webDevicons.get_icon_color_by_filetype(ft, { default = true })
+        if not icon_name then
+          return
+        end
+        local iconSkeleton = {
+          ["BufferLine" .. icon_name .. "Selected"] = {
+            bg = config.transparent_background and "NONE" or theme_palette.tab.activeBackground,
+            fg = icon_color,
+            sp = theme_palette.tab.activeBorder,
+            underline = config.plugins.bufferline.underline_selected
+          },
+          ["BufferLine" .. icon_name] = {
+            bg = theme_palette.tab.inactiveBackground,
+            fg = icon_color,
+          },
+          ["BufferLine" .. icon_name .. "Inactive"] = {
+            bg = config.transparent_background and "NONE" or theme_palette.tab.unfocusedActiveBackground,
+            fg = icon_color,
+            sp = theme_palette.tab.unfocusedActiveBorder,
+            underline = config.plugins.bufferline.underline_selected
+          },
+        }
+        M.initialise(iconSkeleton)
+      end,
+    })
+  end
+end
+
+-- generate list of skeletons from plugin with `attribute` is name of plugin and `value` is plugin highlight
+---@param plugins table
+function M.load_skeletons(plugins)
+  local editor = require("monokai-pro.editor")
+  local syntax = require("monokai-pro.syntax")
+  local semantic_tokens = require("monokai-pro.semantic_tokens")
+  local special = require("monokai-pro.special")
+  local skeletons = {
+    ["editor"] = editor,
+    ["syntax"] = syntax,
+    ["special"] = special,
+    ["semantic_tokens"] = semantic_tokens,
+  }
+
+  for _, p in ipairs(plugins) do
+    local plugin_ok, pluginConfig = pcall(require, "monokai-pro.plugins." .. p)
+    if not plugin_ok then
+      local msg = "There is no highlight config named " .. p
+      local level = "warn"
+      util.notify(msg, level)
+      goto continue
+    end
+    skeletons[p] = pluginConfig
+    ::continue::
+  end
+  return skeletons
 end
 
 return M
