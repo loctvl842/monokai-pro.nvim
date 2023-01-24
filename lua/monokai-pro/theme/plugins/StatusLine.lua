f7759bedac9cb326027a71ee45aeb43e536a76f2
loctvl842 <loclepnvx@gmail.com>
2023-01-24 12:08:53 +0700

    float lualine

--- lua/monokai-pro/theme/plugins/StatusLine.lua
+++ lua/monokai-pro/theme/plugins/StatusLine.lua
@@ -4,78 +4,79 @@ local M = {}
 --- @param config Config
 --- @param hp Helper
 M.setup = function(c, config, hp)
-	return {
-		StatusLine = { bg = c.editorSuggestWidget.background, fg = c.editorSuggestWidget.foreground },
-		StatusLineNC = { bg = c.editorSuggestWidget.foreground, fg = c.editorSuggestWidget.background },
-		StatusLineSeparator = { fg = c.base.black },
-		StatusLineTerm = { fg = c.base.black },
-		StatusLineTermNC = { fg = c.base.black },
-		SLDiffAdd = {
-			bg = c.editor.background,
-			fg = c.gitDecoration.addedResourceForeground,
-		},
-		SLDiffChange = {
-			bg = c.editor.background,
-			fg = c.gitDecoration.modifiedResourceForeground,
-		},
-		SLDiffDelete = {
-			bg = c.editor.background,
-			fg = c.gitDecoration.deletedResourceForeground,
-		},
-		SLGitIcon = {
-			fg = c.base.green,
-			bg = c.editorHoverWidget.background,
-		},
-		SLBranchName = {
-			fg = c.editorHoverWidget.foreground,
-			bg = c.editorHoverWidget.background,
-		},
-		SLSeparator = {
-			fg = c.editorHoverWidget.background,
-			bg = c.editor.background,
-		},
-		SLError = {
-			fg = c.inputValidation.errorForeground,
-			bg = c.editor.background,
-		},
-		SLWarning = {
-			fg = c.inputValidation.warningForeground,
-			bg = c.editor.background,
-		},
-		SLInfo = {
-			bg = c.editor.background,
-			fg = c.inputValidation.infoForeground,
-		},
-		SLPosition = {
-			fg = c.base.magenta,
-			bg = c.editorHoverWidget.background,
-		},
-		SLShiftWidth = {
-			fg = c.base.yellow,
-			bg = c.editorHoverWidget.background,
-		},
-		SLEncoding = {
-			fg = c.base.green,
-			bg = c.editorHoverWidget.background,
-		},
-		SLFiletype = {
-			fg = c.base.cyan,
-			bg = c.editorHoverWidget.background,
-		},
-		SLMode = {
-			fg = c.base.green,
-			bg = c.editorHoverWidget.background,
-			bold = true,
-		},
-		SLSeparatorUnused = {
-			fg = c.editor.background,
-			bg = c.editorHoverWidget.background,
-		},
-		SLPadding = {
-			fg = c.editor.background,
-			bg = c.editor.background,
-		},
-	}
+  local float = true
+  return {
+    StatusLine = { bg = c.editorSuggestWidget.background, fg = c.editorSuggestWidget.foreground },
+    StatusLineNC = { bg = c.editorSuggestWidget.foreground, fg = c.editorSuggestWidget.background },
+    StatusLineSeparator = { fg = c.base.black },
+    StatusLineTerm = { fg = c.base.black },
+    StatusLineTermNC = { fg = c.base.black },
+    SLDiffAdd = {
+      bg = float and c.editor.background or c.sideBar.background,
+      fg = c.gitDecoration.addedResourceForeground,
+    },
+    SLDiffChange = {
+      bg = float and c.editor.background or c.sideBar.background,
+      fg = c.gitDecoration.modifiedResourceForeground,
+    },
+    SLDiffDelete = {
+      bg = float and c.editor.background or c.sideBar.background,
+      fg = c.gitDecoration.deletedResourceForeground,
+    },
+    SLGitIcon = {
+      bg = float and c.editorHoverWidget.background or c.sideBar.background,
+      fg = c.base.green,
+    },
+    SLBranchName = {
+      bg = float and c.editorHoverWidget.background or c.sideBar.background,
+      fg = c.editorHoverWidget.foreground,
+    },
+    SLError = {
+      bg = float and c.editor.background or c.sideBar.background,
+      fg = c.inputValidation.errorForeground,
+    },
+    SLWarning = {
+      bg = float and c.editor.background or c.sideBar.background,
+      fg = c.inputValidation.warningForeground,
+    },
+    SLInfo = {
+      bg = float and c.editor.background or c.sideBar.background,
+      fg = c.inputValidation.infoForeground,
+    },
+    SLPosition = {
+      bg = float and c.editorHoverWidget.background or c.sideBar.background,
+      fg = c.base.magenta,
+    },
+    SLShiftWidth = {
+      bg = float and c.editorHoverWidget.background or c.sideBar.background,
+      fg = c.base.yellow,
+    },
+    SLEncoding = {
+      bg = float and c.editorHoverWidget.background or c.sideBar.background,
+      fg = c.base.green,
+    },
+    SLFiletype = {
+      bg = float and c.editorHoverWidget.background or c.sideBar.background,
+      fg = c.base.cyan,
+    },
+    SLMode = {
+      bg = float and c.editorHoverWidget.background or c.sideBar.background,
+      fg = c.base.green,
+      bold = true,
+    },
+    SLSeparatorUnused = {
+      bg = float and c.editorHoverWidget.background or c.sideBar.background,
+      fg = c.editor.background,
+    },
+    SLSeparator = {
+      bg = float and c.editor.background or c.sideBar.background,
+      fg = float and c.editorHoverWidget.background or c.sideBar.background,
+    },
+    SLPadding = {
+      bg = float and c.editor.background or c.sideBar.background,
+      fg = c.editor.background,
+    },
+  }
 end
 
 return M
