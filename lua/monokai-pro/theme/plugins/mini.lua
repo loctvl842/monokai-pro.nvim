7d15f9b3be1129ed3a7fdaa8dbdcf04c627927c9
Ethan Schrunk <ethan.schrunk@knowledgelake.com>
2024-09-13 15:18:32 -0500

    Add definitions for mini.statusline colors

--- lua/monokai-pro/theme/plugins/mini.lua
+++ lua/monokai-pro/theme/plugins/mini.lua
@@ -5,6 +5,14 @@ local M = {}
 function M.get(c, _, _)
   return {
     MiniIndentscopeSymbol = { fg = c.base.blue },
+
+    -- mini.status
+    MiniStatuslineModeNormal = { fg = c.base.black, bg = c.base.green },
+    MiniStatuslineModeInsert = { fg = c.base.black, bg = c.base.yellow },
+    MiniStatuslineModeCommand = { fg = c.base.black, bg = c.base.red },
+    MiniStatuslineModeVisual = { fg = c.base.black, bg = c.base.cyan },
+    MiniStatuslineModeReplace = { fg = c.base.black, bg = c.base.cyan },
+    MiniStatuslineModeOther = { fg = c.base.black, bg = c.base.white },
   }
 end
 
