abfe4fbf6ce9c675999752da60b92233113cc4aa
Greg Helding <158190040+gthelding@users.noreply.github.com>
2025-07-27 15:16:13 -0500

    Update ristretto.lua

--- lua/monokai-pro/colorscheme/palette/ristretto.lua
+++ lua/monokai-pro/colorscheme/palette/ristretto.lua
@@ -12,8 +12,7 @@ return {
   accent6 = "#a8a9eb",
   dimmed1 = "#c3b7b8",
   dimmed2 = "#948a8b",
-  --dimmed3 = "#72696a",
+  dimmed3 = "#72696a",
   dimmed4 = "#5b5353",
   dimmed5 = "#403838",
-  dimmed3 = "#e6d9db",
 }
