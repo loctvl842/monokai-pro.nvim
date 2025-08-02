243cf826cd75400fa8dc95c2a27f64ea34c288a8
Greg Helding <158190040+gthelding@users.noreply.github.com>
2025-08-01 23:51:11 -0500

    Create highlights.lua

---
+++ lua/monokai-pro/highlights.lua
@@ -0,0 +1,8 @@
+MiniIconsGrey   = { fg = "#948a8b" },
+MiniIconsRed    = { fg = "#fd6883" },
+MiniIconsBlue   = { fg = "#85dacc" },
+MiniIconsGreen  = { fg = "#adda78" },
+MiniIconsYellow = { fg = "#f9cc6c" },
+MiniIconsOrange = { fg = "#f38d70" },
+MiniIconsPurple = { fg = "#a8a9eb" },
+MiniIconsCyan   = { fg = "#85dacc" },
