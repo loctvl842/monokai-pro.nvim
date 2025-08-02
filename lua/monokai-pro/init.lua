21853235f9c2a5206d8d14a13f5263efe4c56f98
Greg Helding <158190040+gthelding@users.noreply.github.com>
2025-08-02 00:12:22 -0500

    Update init.lua

--- lua/monokai-pro/init.lua
+++ lua/monokai-pro/init.lua
@@ -3,12 +3,12 @@ local util = require("monokai-pro.util")
 local theme = require("monokai-pro.theme")
 local command = require("monokai-pro.command")
 local highlights = require("monokai-pro.highlights")
+
 ---@class MonokaiPro
 local M = {}
 
 M.load = function()
   util.theme.load(theme.setup())
-  local highlights = require("monokai-pro.highlights")
   for group, opts in pairs(highlights) do
     vim.api.nvim_set_hl(0, group, opts)
   end
