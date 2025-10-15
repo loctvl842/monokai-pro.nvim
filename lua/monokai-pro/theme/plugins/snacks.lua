701dd784900c4a0114458d508cc9cf75105b0d24
Lucas Crownover <lcrownover127@gmail.com>
2025-10-15 14:46:14 -0700

    updated snacks highlighting to increase contrast on some unreadable text

--- lua/monokai-pro/theme/plugins/snacks.lua
+++ lua/monokai-pro/theme/plugins/snacks.lua
@@ -6,6 +6,18 @@ function M.get(c, _, hp)
     SnacksDashboardNormal = { bg = c.editor.background, fg = c.editor.foreground },
     SnacksDashboardDesc = { fg = c.base.dimmed1 },
     SnacksDashboardIcon = { fg = c.base.blue },
+    SnacksPickerDir = { fg = c.base.dimmed1 },
+    SnacksPickerFile = { fg = c.base.white },
+    SnacksWinKeySep = { fg = c.base.dimmed1 },
+    SnacksImageLoading = { fg = c.base.dimmed1 },
+    SnacksPickerTotals = { fg = c.base.dimmed1 },
+    SnacksPickerBufFlags = { fg = c.base.dimmed1 },
+    SnacksPickerKeymapRhs = { fg = c.base.dimmed1 },
+    SnacksPickerPathHidden = { fg = c.base.dimmed1 },
+    SnacksPickerUnselected = { fg = c.base.dimmed1 },
+    SnacksPickerPathIgnored = { fg = c.base.dimmed1 },
+    SnacksPickerGitStatusIngnored = { fg = c.base.dimmed1 },
+    SnacksPickerGitStatusUntracked = { fg = c.base.dimmed1 },
   }
 end
 
