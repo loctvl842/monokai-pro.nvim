ffb6586648a2bfda65f661b8ea0c7a5d9624f5c3
Anson Heung <anson.heung@bowtie.com.hk>
2026-02-01 22:33:04 +0800

    fix: broken rainbow-delimeters

--- lua/monokai-pro/theme/plugins/rainbow-delimeters.lua
+++ lua/monokai-pro/theme/plugins/rainbow-delimeters.lua
@@ -5,9 +5,13 @@ return {
 
   highlights = function(c)
     return {
-      AlphaButton = { fg = c.base.magenta },
-      AlphaFooter = { fg = c.base.blue },
-      AlphaHeader = { fg = c.base.yellow },
+      RainbowDelimiterRed = { fg = c.base.red },
+      RainbowDelimiterYellow = { fg = c.base.yellow },
+      RainbowDelimiterBlue = { fg = c.base.cyan },
+      RainbowDelimiterOrange = { fg = c.base.blue },
+      RainbowDelimiterGreen = { fg = c.base.green },
+      RainbowDelimiterViolet = { fg = c.base.magenta },
+      RainbowDelimiterCyan = { fg = c.base.cyan },
     }
   end,
 }
