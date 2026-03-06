88cf9cf1c259c1e8c904f0680c8efd819d49541a
jakesurry <jacobsurry05@gmail.com>
2026-03-05 21:57:14 -0800

    Refactor ~/plugins/rainbow-delimiters.lua to better match colorscheme

--- lua/monokai-pro/theme/plugins/rainbow-delimiters.lua
+++ lua/monokai-pro/theme/plugins/rainbow-delimiters.lua
@@ -6,12 +6,11 @@ return {
   highlights = function(c)
     return {
       RainbowDelimiterRed = { fg = c.base.red },
-      RainbowDelimiterYellow = { fg = c.base.yellow },
-      RainbowDelimiterBlue = { fg = c.base.cyan },
       RainbowDelimiterOrange = { fg = c.base.blue },
+      RainbowDelimiterYellow = { fg = c.base.yellow },
       RainbowDelimiterGreen = { fg = c.base.green },
-      RainbowDelimiterViolet = { fg = c.base.magenta },
       RainbowDelimiterCyan = { fg = c.base.cyan },
+      RainbowDelimiterViolet = { fg = c.base.magenta },
     }
   end,
 }
